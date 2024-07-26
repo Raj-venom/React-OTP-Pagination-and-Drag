5aaf21cf833f3825f48180fee85ff6a23329ad82
Raj-venom <rajsingh.rsr9824@gmail.com>
2024-07-26 15:47:24 +0545

    Add routing

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,18 @@ import List from './components/drag/List'
 import Drag from './page/drag'
 import Otp from "./page/otp"
 import Table from './page/table'
+import { Outlet } from 'react-router-dom'
+
 
 function App() {
 
   return (
     <>
-    {/* <Otp/> */}
-    {/* <Drag/> */}
-    <Table/>
+      {/* <Otp/> */}
+      {/* <Drag/> */}
+      {/* <Table/> */}
+      <Outlet />
+
     </>
 
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,38 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import Otp from './page/otp.jsx'
+import Drag from './page/drag.jsx'
+import Table from './page/table.jsx'
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <Otp />
+      },
+      {
+        path: "/otp-form",
+        element: <Otp />
+      },
+      {
+        path: "/course-list",
+        element: <Drag />
+      },
+      {
+        path: "/batches",
+        element: <Table />
+      },
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
